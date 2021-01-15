7fe7a91949a2cb5bce236ce965f5e462fdf8b830
Christian Campos <18518@my4county.net>
2021-01-15 12:33:47 -0500

    modified css and js

--- bottles.js
+++ bottles.js
@@ -4,22 +4,22 @@ var count = 99;
 
 while (count > 0)
 {
-	document.write(count + " " + word + " of pop on the wall, ");
+	document.write(count + " " + word + " of pop on the wall,");
 	document.write("<br>");
 	document.write(count + " " + word + " of pop,");
 	document.write("<br>");
-	document.write(" Take one down, pass it around, ");
+	document.write("Take one down, pass it around,");
 	document.write("<br>");
 	count = count - 1; 
 
 	if(count > 0)
 	{
-		document.write(count + " " + word +" of pop on the wall. ");
+		document.write(count + " " + word +" of pop on the wall.");
 		document.write("<br>");
 	}
 	else
 	{
-		document.write("No more " + word + " of pop on the wall ");
+		document.write("No more " + word + " of pop on the wall");
 		document.write("<br>");
 	}
 	
